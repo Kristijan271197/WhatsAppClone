44dd909c44c21bd86272c261beb55b51a0f06c79
stoyanoski.k <kristijanstojanoski@Yahoo.com>
2020-11-26 11:34:41 +0300

    Registration Activity

--- app/src/main/java/com/invictastudios/whatsappclone/RegistrationActivity.java
+++ app/src/main/java/com/invictastudios/whatsappclone/RegistrationActivity.java
@@ -1,17 +1,30 @@
 package com.invictastudios.whatsappclone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText usernameEditText;
     private EditText passwordEditText;
     private EditText emailEditText;
-    private Button registrationButton;
+
+    private FirebaseAuth auth;
+    private DatabaseReference myRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +34,51 @@ public class RegistrationActivity extends AppCompatActivity {
         usernameEditText = findViewById(R.id.username_edit_text);
         passwordEditText = findViewById(R.id.password_edit_text);
         emailEditText = findViewById(R.id.email_edit_text);
-        registrationButton = findViewById(R.id.registration_button);
+        Button registrationButton = findViewById(R.id.registration_button);
 
+        auth = FirebaseAuth.getInstance();
+
+        registrationButton.setOnClickListener(v -> {
+            String username_text = usernameEditText.getText().toString();
+            String email_text = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            if (TextUtils.isEmpty(username_text) || TextUtils.isEmpty(email_text) || TextUtils.isEmpty(password))
+                Toast.makeText(this, "Please Fill All Fields", Toast.LENGTH_SHORT).show();
+            else
+                RegisterNow(username_text, email_text, password);
+        });
+
+    }
+
+    private void RegisterNow(final String username, String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser firebaseUser = auth.getCurrentUser();
+                        String userId = null;
+                        if (firebaseUser != null) {
+                            userId = firebaseUser.getUid();
+                            myRef = FirebaseDatabase.getInstance()
+                                    .getReference("MyUsers")
+                                    .child(userId);
+                        }
+                        HashMap<String, String> hashMap = new HashMap<>();
+                        hashMap.put("id", userId);
+                        hashMap.put("username", username);
+                        hashMap.put("imageURL", "default");
+
+                        //Opening MainActivity after Success Registration
+                        myRef.setValue(hashMap).addOnCompleteListener(task1 -> {
+                            if (task1.isSuccessful()) {
+                                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+                    } else
+                        Toast.makeText(RegistrationActivity.this, "Invalid Email or Password", Toast.LENGTH_SHORT).show();
+                });
     }
 }
